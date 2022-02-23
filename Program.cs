4e9adbbde8b41067d4f3dd06fd37cce290a9b1fa
Saifune87 <saifune@gmail.com>
2022-02-23 15:22:18 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Threading.Tasks;
+using Tweetinvi;
+using Tweetinvi.Models;
+using Tweetinvi.Parameters;
+
+namespace TwitterV2PicBot
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //Auth.
+            var key = ConfigurationManager.AppSettings["ConsumerAPIKey"];
+            var secret = ConfigurationManager.AppSettings["ConsumerSecret"];
+            var at = ConfigurationManager.AppSettings["AccessToken"];
+            var ats = ConfigurationManager.AppSettings["AccessTokenSecret"];
+            TwitterClient UserClient = new TwitterClient(key, secret, at, ats);
+
+            //Connect image.
+            byte[] ImageBytes = File.ReadAllBytes("charizard.jpg"); //--\TwitterV2PicBot\bin\Debug\net5.0\"
+
+            IMedia ImageIMedia = await UserClient.Upload.UploadTweetImageAsync(ImageBytes);
+
+            //Send image with Parameters.
+            ITweet TweetWithImage = await UserClient.Tweets.PublishTweetAsync(
+                new PublishTweetParameters("#PokeTest #CuteSalamandar #Salamandar") { Medias = { ImageIMedia } });
+
+        }
+
+
+    }
+
+}
